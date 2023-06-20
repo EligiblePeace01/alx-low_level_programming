1518b1b84e07d1d6d51269e9d6f304e033b54a53
EligiblePeace01 <enwerempeace@gmail.com>
2023-06-20 11:21:22 -0700

    program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point of my code
- *
- * Return: Always 0
- */
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
 	_putchar('_');
