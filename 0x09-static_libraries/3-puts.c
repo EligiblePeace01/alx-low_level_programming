0f8764d7d854034bededa3f9622e7aa15093155a
EligiblePeace01 <enwerempeace@gmail.com>
2023-07-07 11:16:31 -0700

    my static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * _puts - prints string followed by newline.
- * @str: pointer to string.
- *
- * Return: void.
+ * _puts - prints an entire string.
+ * @str: tested string
+ * Return: always 0.
  */
-
 void _puts(char *str)
 {
 	int i;
 
 	i = 0;
-	while (*str != '\0')
+
+	while (*(str + i) != '\0')
 	{
-		_putchar(*str);
+		_putchar(*(str + i));
 		i++;
-		str++;
 	}
 	_putchar('\n');
 }
