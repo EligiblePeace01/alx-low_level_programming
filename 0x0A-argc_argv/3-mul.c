aea140bde695020e5145532f3af4aa2b7eca155b
EligiblePeace01 <enwerempeace@gmail.com>
2023-07-10 00:26:14 -0700

    program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,31 +2,28 @@
 #include "main.h"
 
 /**
- * main - Program that takes two integer arguments and prints the product
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if not enough arguments passed in, 0 otherwise
+ * main - Entry Point
+ * atoi is a function that converts a string into an int
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int i, res = 1;
 
-	if (argc == 1 || argc == 2)
+	if (argc != 3)
 	{
-		printf("Error\n");
+		printf("%s\n", "Error");
 		return (1);
 	}
 	else
 	{
-		j = 1;
-
-		for (i = 1; i < 3; i++)
-		j *= atoi(argv[i]);
-
-		printf("%d\n", j);
+		for (i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);
+		}
+		printf("%d\n", res);
 	}
-
 	return (0);
 }
-
