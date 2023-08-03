986587449e0f3ddfd20a1ec18ac1d0d04e57d421
EligiblePeace01 <enwerempeace@gmail.com>
2023-08-02 20:41:22 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int n;
-       	n = 0;
+	register size_t count = 0;
 
 	while (h)
-	{	n++;
-		printf("%d\n", h->n);
+	{
+		printf("%i\n", h->n);
 		h = h->next;
+		count++;
 	}
-	return (n);
+	return (count);
 }
